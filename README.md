99c48b036e739f4c4755e52e8b1b5f4ef7dbb082
waricoma <waricoma@devne.co>
2019-06-26 13:24:30 +0700

    :tada: - Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# dachshund-generator
+
+[![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg?style=flat-square)](https://github.com/Flet/semistandard)
+
+on Slack bot.
+
+<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vS0mNPHQeOwcNAAweIO5gUuKSEZkZPXwFBtwQe6FZ_ByEd2udL7pUPFZEmh8-4wf8QizJW1QfP-cr4W/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * @fileOverview index.js
+ *
+ * @author Motone Adachi (@waritocomatta)
+ * @version 1.0.0
+ */
+
+require('dotenv').config();
+const http = require('http');
+const { RTMClient, WebClient } = require('@slack/client');
+const rtmClient = new RTMClient(process.env.TOKEN);
+const webClient = new WebClient(process.env.TOKEN);
+
+// Slack Current Channel Monitoring.
+rtmClient.on('message', event => {
+  console.log(event);
+  // is there text?
+  if (!('text' in event)) {
+    return;
+  }
+
+  // Validation
+  if (!(event.text.match(/^:dachshund:( |)([0-9]|[０-９])+$/))) {
+    return;
+  }
+
+  const bodyCount = parseFloat(event.text.split('dachshund:')[1].trim().replace(/[Ａ-Ｚａ-ｚ０-９]/g, str => {
+    return String.fromCharCode(str.charCodeAt(0) - 65248);
+  }));
+
+  // Generate
+  let dachshund = ':dachshund-backend:';
+  for (let i = 0; i < bodyCount; i++) {
+    dachshund += ':dachshund-body:';
+  }
+  dachshund += ':dachshund-frontend:';
+
+  // Reply
+  webClient.chat.postMessage({
+    channel: event.channel,
+    text: dachshund,
+    username: process.env.USERNAME,
+    icon_emoji: `:dachshund:`
+  });
+});
+rtmClient.start();
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('Dachshund Generator on Slack. 🐕🐕🐕');
+  res.end();
+}).listen(process.env.PORT, '0.0.0.0', () => console.log(`Server running at ${process.env.PORT}`));
